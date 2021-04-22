9bdf27c0e25aeb8f3b8271e03c332ac4bc0fc62c
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-04-22 18:18:20 -0400

    Initial Project setup.

---
+++ src/main/java/com/alphacoder/springsecuritycustomauthenticationfilter/SpringSecurityCustomAuthenticationFilterApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder.springsecuritycustomauthenticationfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomAuthenticationFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCustomAuthenticationFilterApplication.class, args);
+	}
+
+}
